83e0d4e71c23a393294255664fbdfbf7e00cfff5
rjs1197 <gmj1197@gmail.com>
2017-11-05 18:53:45 +0900

    [Modified] opencv function for RGB to gray

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,11 +1,17 @@
 #include <jni.h>
 #include <string>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+using namespace cv;
 
 extern "C"
-JNIEXPORT jstring JNICALL
-Java_mjh_v01_aproject_MainActivity_stringFromJNI(
-        JNIEnv *env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+JNIEXPORT void JNICALL
+Java_mjh_v01_aproject_MyVideoView_ConvertRGBtoGray(JNIEnv *env, jobject instance,
+                                                   jlong matAddrInput, jlong matAddrResult) {
+
+    Mat &matInput = *(Mat *)matAddrInput;
+    Mat &matResult = *(Mat *)matAddrResult;
+
+    cvtColor(matInput, matResult, CV_RGBA2GRAY);
 }
