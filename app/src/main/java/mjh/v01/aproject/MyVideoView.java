702f91e13c84ea14f3d816a33ae31830fb874c78
hojihun5516 <hojihun5516@daum.net>
2017-11-08 22:49:23 +0900

    [Modified] video class

--- app/src/main/java/mjh/v01/aproject/MyVideoView.java
+++ app/src/main/java/mjh/v01/aproject/MyVideoView.java
@@ -5,6 +5,7 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.res.Configuration;
 import android.hardware.Camera;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
@@ -18,6 +19,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
@@ -47,12 +49,15 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import mjh.v01.aproject.VO.VideoInfo;
 
-public class MyVideoView extends AppCompatActivity {
+import static android.view.View.resolveSize;
+
+public class MyVideoView extends AppCompatActivity implements SurfaceHolder.Callback{
 
     public static final String TAG = "MainActivity";
     private static String EXTERNAL_STORAGE_PATH = "";
@@ -64,7 +69,6 @@ public class MyVideoView extends AppCompatActivity {
 
     MediaPlayer player;
     MediaRecorder recorder;
-    private Camera camera = null;
     UploadTask uploadTask;
     SurfaceHolder holder;
     @BindView(R.id.btnRecord)
@@ -82,6 +86,12 @@ public class MyVideoView extends AppCompatActivity {
     String email;
     int index;
     Uri videoUri;
+    Camera mCamera;
+    SurfaceView surface;
+    public List<Camera.Size> listPreviewSizes;
+    private Camera.Size previewSize;
+
+
 
 
     @Override
@@ -94,55 +104,19 @@ public class MyVideoView extends AppCompatActivity {
         final StorageReference storageRef = FirebaseStorage.getInstance().getReferenceFromUrl("gs://aproject-4e6d6.appspot.com/");
         firebaseDatabase = FirebaseDatabase.getInstance();
         databaseReference = firebaseDatabase.getReference();
+        //Determine whether external memory is available
         String state = Environment.getExternalStorageState();
         if (!state.equals(Environment.MEDIA_MOUNTED)) {
             Log.d(TAG, "External Storage Media is not mounted.");
         } else {
             EXTERNAL_STORAGE_PATH = Environment.getExternalStorageDirectory().getAbsolutePath();
         }
-
-        // create a SurfaceView instance and add it to the layout
-        /*class Preview extends SurfaceView implements SurfaceHolder.Callback {
-            SurfaceHolder mHolder;
-
-            Preview(Context context) {
-                super(context);
-
-                mHolder = getHolder();
-                mHolder.addCallback(this);
-                mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
-            }
-
-
-
-            public void surfaceCreated(SurfaceHolder holder) {
-                // Surface가 생성되었으니 프리뷰를 어디에 띄울지 지정해준다. (holder 로 받은 SurfaceHolder에 뿌려준다.
-
-                //출처: http://rinear.tistory.com/entry/AndroidSurfaceView-Camera-Preview카메라-프리뷰 [괴도군의 블로그]
-            }
-
-            public void surfaceDestroyed(SurfaceHolder holder) {
-                // 프리뷰 제거시 카메라 사용도 끝났다고 간주하여 리소스를 전부 반환한다
-
-//                출처: http://rinear.tistory.com/entry/AndroidSurfaceView-Camera-Preview카메라-프리뷰 [괴도군의 블로그]
-            }
-
-            public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
-                // 프리뷰를 회전시키거나 변경시 처리를 여기서 해준다.
-                // 프리뷰 변경시에는 먼저 프리뷰를 멈춘다음 변경해야한다.
-
-
-//                출처: http://rinear.tistory.com/entry/AndroidSurfaceView-Camera-Preview카메라-프리뷰 [괴도군의 블로그]
-            }
-        }
-*/
-//        Preview surface = new Preview(this);
-        SurfaceView surface = new SurfaceView(this);
-        holder = surface.getHolder();
-        holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
-
+        //Create a surface view, assign it to the holder, and add it to the frame.
+        surface = new SurfaceView(this);
+        init();
+        /*holder = surface.getHolder();
+        holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);*/
         frame.addView(surface);
-
 
         btnRecord.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -150,16 +124,19 @@ public class MyVideoView extends AppCompatActivity {
                     if (recorder == null) {
                         recorder = new MediaRecorder();
                     }
+                    //In the recorder, put in the items that I need to put
                     recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
                     recorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
                     recorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
                     recorder.setAudioEncoder(MediaRecorder.AudioEncoder.DEFAULT);
                     recorder.setVideoEncoder(MediaRecorder.VideoEncoder.DEFAULT);
+                    recorder.setOrientationHint(90);
                     filename = createFilename();
                     Log.d(TAG, "current filename : " + filename);
 
                     recorder.setOutputFile(filename);
                     recorder.setPreviewDisplay(holder.getSurface());
+
                     recorder.prepare();
                     recorder.start();
 
@@ -177,6 +154,7 @@ public class MyVideoView extends AppCompatActivity {
             public void onClick(View v) {
                 if (recorder == null)
                     return;
+                //Release methods on the recorder
                 recorder.stop();
                 recorder.reset();
                 recorder.release();
@@ -189,6 +167,7 @@ public class MyVideoView extends AppCompatActivity {
                 values.put(MediaStore.MediaColumns.DATE_ADDED, System.currentTimeMillis() / 1000);
                 values.put(MediaStore.MediaColumns.MIME_TYPE, "mp4");
                 values.put(MediaStore.Audio.Media.DATA, filename);
+                // uri에 인서트 시켜준다
                 videoUri = getContentResolver().insert(MediaStore.Video.Media.EXTERNAL_CONTENT_URI, values);
                 if (videoUri == null) {
                     Log.d("SampleVideoRecorder", "Video insert failed.");
@@ -197,6 +176,7 @@ public class MyVideoView extends AppCompatActivity {
                 }
                 Log.d("Whatisthis", videoUri.getLastPathSegment());
                 Toast.makeText(MyVideoView.this, "Success", Toast.LENGTH_SHORT).show();
+                //미디어 스캐너로 하여금 Uri에 대한 파일을 스캔하고 미디어 라이브러리에 파일을 추가하도록 해줍니다.
                 sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, videoUri));
             }
         });
@@ -209,6 +189,7 @@ public class MyVideoView extends AppCompatActivity {
 
 
                 try {
+                    //경로에 있는 내 파일을 가져와서 실행시킨다
                     FileInputStream fs = new FileInputStream(new File(filename));
                     FileDescriptor fd = fs.getFD();
                     player.setDataSource(fd);
@@ -246,10 +227,12 @@ public class MyVideoView extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Log.d("UPload", "update setOnClickListener");
-                //progressDialog를 띄어주고
+                //progressDialog를 띄어줄까 고민중
 
                 Log.d(" tPathSegment()", videoUri.getLastPathSegment());
+                //storage path
                 StorageReference riversRef = storageRef.child("video/" + email + "/" + videoUri.getLastPathSegment());
+                //upload start Fail or Success Listener
                 uploadTask = riversRef.putFile(videoUri);
                 uploadTask.addOnFailureListener(new OnFailureListener() {
                     @Override
@@ -326,7 +309,7 @@ public class MyVideoView extends AppCompatActivity {
     }
 
 
-    protected void onPause() {
+    /*protected void onPause() {
         if (camera != null) {
             camera.release();
             camera = null;
@@ -344,11 +327,13 @@ public class MyVideoView extends AppCompatActivity {
 
         super.onPause();
     }
-
+*/
     private void setCount() {
+        //get data in firebase
         databaseReference.child("VIDEO").child(email).child("count").runTransaction(new Transaction.Handler() {
             @Override
             public Transaction.Result doTransaction(MutableData mutableData) {
+                //getValue is my count
                 if (mutableData.getValue() == null) {
                     mutableData.setValue(1);
 
@@ -359,7 +344,7 @@ public class MyVideoView extends AppCompatActivity {
                 }
                 return Transaction.success(mutableData);
             }
-
+            //This will be called if Transaction.Result doTransaction succeeds.
             @Override
             public void onComplete(DatabaseError databaseError, boolean success, DataSnapshot dataSnapshot) {
                 // Analyse databaseError for any error during increment
@@ -374,11 +359,88 @@ public class MyVideoView extends AppCompatActivity {
     }
 
     private void updateDB() {
+        //update to firebase DB
         VideoInfo videoInfo = new VideoInfo(videoUri.getLastPathSegment(), 00);
         databaseReference.child("VIDEO").child(email).child(String.valueOf(index)).setValue(videoInfo);
-        //��� push����
+        //finish and go to MainActivity
         Intent intent = new Intent(MyVideoView.this, MainActivity.class);
         intent.putExtra("email", email);
         startActivity(intent);
     }
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
+    @Override
+    public void surfaceCreated(SurfaceHolder surfaceHolder) {
+        try {
+            // 카메라 객체를 사용할 수 있게 연결한다.
+            if(mCamera  == null){
+                mCamera  = Camera.open();
+            }
+
+            // 카메라 설정
+            Camera.Parameters parameters = mCamera .getParameters();
+
+            // 카메라의 회전이 가로/세로일때 화면을 설정한다.
+            if (getResources().getConfiguration().orientation != Configuration.ORIENTATION_LANDSCAPE) {
+                parameters.set("orientation", "portrait");
+                mCamera.setDisplayOrientation(90);
+                parameters.setRotation(90);
+                Log.d("ifResource","ifResource");
+            } else {
+                parameters.set("orientation", "landscape");
+                mCamera.setDisplayOrientation(0);
+                parameters.setRotation(0);
+                Log.d("elseResource","elseResource");
+            }
+            mCamera.setParameters(parameters);
+
+            mCamera.setPreviewDisplay(surfaceHolder);
+
+            // 카메라 미리보기를 시작한다.
+            mCamera.startPreview();
+
+            // 자동포커스 설정
+            mCamera.autoFocus(new Camera.AutoFocusCallback() {
+                public void onAutoFocus(boolean success, Camera camera) {
+                    if (success) {
+
+                    }
+                }
+            });
+        } catch (IOException e) {
+        }
+    }
+    // SurfaceView 의 크기가 바뀌면 호출
+    @Override
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int w, int h) {
+
+        // 카메라 화면을 회전 할 때의 처리
+        if (surfaceHolder.getSurface() == null){
+            // 프리뷰가 존재하지 않을때
+            return;
+        }
+
+    }
+
+    // SurfaceView가 종료시 호출
+    @Override
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+        if(mCamera != null){
+            // 카메라 미리보기를 종료한다.
+            mCamera.stopPreview();
+            mCamera.release();
+            mCamera = null;
+        }
+    }
+    private void init(){
+        //Default status camera
+        mCamera = Camera.open();
+        mCamera.setDisplayOrientation(90);
+
+        // surfaceview setting
+        holder = surface.getHolder();
+        holder .addCallback(this);
+        holder .setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+    }
+
 }
